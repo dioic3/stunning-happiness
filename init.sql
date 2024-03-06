63a3bf23132d74eb9d16bef880e42faf7c54de3e
PedroBorem <pedro.borem@outlook.com>
2024-03-06 03:02:21 -0300

    [database] mudei a criacao da tabela e tirei o select

--- init.sql
+++ init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS quiz;
 USE quiz;
-CREATE TABLE questions (
+
+CREATE TABLE IF NOT EXISTS questions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     question TEXT,
     option_a VARCHAR(255),
@@ -9,15 +10,10 @@ CREATE TABLE questions (
     option_d VARCHAR(255),
     correct_answer CHAR(1)
 );
-SELECT * FROM questions LIMIT 10;
+
 INSERT INTO questions (question, option_a, option_b, option_c, option_d, correct_answer) 
 VALUES 
 ('Qual é a capital do Brasil?', 'Buenos Aires', 'Rio de Janeiro', 'São Paulo', 'Brasília', 'd'),
 ('Qual é a capital do Japão?', 'Beijing', 'Seoul', 'Tokyo', 'Shanghai', 'c'),
 ('Qual é a capital da Rússia?', 'Moscou', 'São Petersburgo', 'Kiev', 'Vladivostok', 'a'),
 ('Qual é a capital da Austrália?', 'Sydney', 'Melbourne', 'Canberra', 'Brisbane', 'c');
-
-
-
-
-
